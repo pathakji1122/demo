2b8920102ffc407340ae20a9961c0c8a722d691e
prate <prateekprakash11@gmail.con>
2023-01-11 14:24:29 +0530

    git init

--- demo/src/main/java/com/example/demo/StudentController.java
+++ demo/src/main/java/com/example/demo/StudentController.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,6 +30,41 @@ public class StudentController {
         return "Deleted Successfully";
     }
 
-}
+    @PostMapping("/users/register")
+    public void registerStudent(@Validated @RequestBody Student newStudent) {
+        List<Student> users = studentrepository.findAll();
+        System.out.println("New user: " + newStudent.toString());
+        for (Student user : users) {
+            if (user.equals(newStudent)) {
+                System.out.println("User Already exists!");
+            }
+        }
+        studentrepository.save(newStudent);
+    }
+    @PostMapping("/users/login")
+    public void loginUser(@Validated @RequestBody Student student) {
+        List<Student> users = studentrepository.findAll();
+        for (Student other : users) {
+            if (other.equals(student)) {
+                studentrepository.save(student);
+            }
+
+        }
+    }
+        @PostMapping("/users/logout")
+        public void logoutUser(@Validated @RequestBody Student student){
+            List<Student> users1 = studentrepository.findAll();
+            for (Student other : users1) {
+                if (other.equals(student)) {
+                    studentrepository.save(student);
+                }
+            }
+
+        }
+    }
+
+
+
+
 
 
